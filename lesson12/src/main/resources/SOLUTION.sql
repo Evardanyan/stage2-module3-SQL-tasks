d3ffa6346cea74f7197345d14f1b0f76e30f29b4
Edgar Vardanyan <e.vardan4an@gmail.com>
2022-11-30 01:38:37 +0400

    solved third try

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-SET REFERENTIAL_INTEGRITY FALSE; DELETE FROM student WHERE id IN (SELECT mark.id FROM subject JOIN mark ON mark.id = subject.id WHERE subject.grade > 4);
-SET REFERENTIAL_INTEGRITY FALSE; DELETE FROM student WHERE id IN (SELECT  s.id FROM student AS s JOIN mark AS m ON s.id = m.student_id WHERE m.mark < 4);
-SET REFERENTIAL_INTEGRITY FALSE; DELETE FROM paymenttype WHERE name = 'DAILY';
-SET REFERENTIAL_INTEGRITY FALSE; DELETE FROM mark WHERE mark < 7;
+DELETE FROM student WHERE id IN (SELECT DISTINCT mark.student_id FROM mark  JOIN subject  ON subject.id = mark.subject_id WHERE grade >= 4);
+DELETE FROM student WHERE id IN (SELECT  DISTINCT student.id FROM student  JOIN mark  ON student.id = mark.student_id WHERE mark.mark < 4);
+DELETE FROM paymenttype WHERE name = 'DAILY';
+DELETE FROM mark WHERE mark < 7;
